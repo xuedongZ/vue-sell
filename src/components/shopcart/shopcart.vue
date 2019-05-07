a3dfbf45204174ac616ca5f92b426a753aa95a29
xuedongZ <xuedong6624@gmail.com>
2019-05-07 21:54:26 +0800

    finish shopcart-list

--- src/components/shopcart/shopcart.vue
+++ src/components/shopcart/shopcart.vue
@@ -23,17 +23,17 @@
           <div class="inner inner-hook"></div>
         </div>
       </div>
-      <div class="shopcart-list" v-show="listShow">
+      <div class="shopcart-list" v-show="listShow" transition="fold">
         <div class="list-header">
           <h1 class="title">购物车</h1>
-          <span class="empty">清空</span>
+          <span class="empty" @click="empty">清空</span>
         </div>
-        <div class="list-content">
+        <div class="list-content" v-el:list-content>
           <ul>
             <li class="food" v-for="food in selectFoods">
               <span class="name">{{food.name}}</span>
               <div class="price">
-                <span class="¥{{food.price*food.count}}"></span>
+                <span>¥{{food.price*food.count}}</span>
               </div>
               <div class="cartcontrol-warpper">
                 <cartcontrol :food="food"></cartcontrol>
@@ -48,6 +48,7 @@
 </template>
 
 <script type="text/ecmascript-6">
+import BScroll from 'better-scroll';
 import cartcontrol from 'components/cartcontrol/cartcontrol';
 
 export default {
@@ -133,6 +134,17 @@ export default {
         return false;
       }
       let show = !this.fold;
+      if (show) {
+        this.$nextTick(() => {
+          if (!this.scroll) {
+            this.scroll = new BScroll(this.$els.listContent, {
+              click: true
+            });
+          } else {
+            this.scroll.refresh();
+          }
+        });
+      }
       return show;
     }
   },
@@ -153,6 +165,11 @@ export default {
         return;
       }
       this.fold = !this.fold;
+    },
+    empty() {
+      this.selectFoods.forEach(food => {
+        food.count = 0;
+      });
     }
   },
   transitions: {
@@ -308,4 +325,63 @@ export default {
             border-radius: 50%  // 圆
             background: rgb(0,160,220)   // 购物车的蓝色
             transition: all 0.4s linear
+    .shopcart-list
+      position: absolute
+      left: 0
+      top: 0
+      z-index: -1
+      width: 100%
+      &.fold-transition
+        transition: all 0.5s
+        transform: translate3d(0,-100%,0)  // 最终状态，技巧，-100%，相对于当前状态自身的高度做一个偏移。
+      &.fold-enter, &.fold-leave
+        transform: translate3d(0,0,0)
+      .list-header
+        height: 40px
+        line-height: 40px
+        padding: 0 18px
+        background: #f3f5f7
+        border-bottom: 1px solid rgba(7,17,27,0.1)
+        .title
+          float: left
+          font-size: 14px
+          color: rgb(7,17,27)
+        .empty
+          float: right
+          font-size: 12px
+          color: rgb(0,160,220)
+      .list-content
+        padding: 0 18px
+        max-height: 217px
+        overflow: hidden
+        background: #fff
+        .food
+          position: relative
+          padding: 12px 0
+          box-sizing: border-box
+          border-1px(rgba(7,17,27,0.1))
+          .name
+            line-height: 24px
+            font-size: 14px
+            color: rgb(7,17,27)
+          .price
+            position: absolute
+            right: 90px
+            bottom: 12px
+            line-height: 24px
+            font-size: 14px
+            font-weight: 700
+            color: rgb(240,20,20)
+          .cartcontrol-wrapper
+            position: absolute
+            right: 0
+            bottom: 6px
+
+
+
+
+
+
+
+
 </style>
