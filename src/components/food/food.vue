3bfd954903417f1eecfaa37edfaf7ff09a81983c
xuedongZ <xuedong6624@gmail.com>
2019-05-12 00:54:28 +0800

    finish split & raingsleck block style

--- src/components/food/food.vue
+++ src/components/food/food.vue
@@ -16,19 +16,34 @@
         <div class="price">
           <span class="now">¥{{food.price}}</span><span class="old" v-show="food.oldPrice">¥{{food.oldPrice}}</span>
         </div>
+        <div class="cartcontrol-wrapper">
+          <cartcontrol :food="food"></cartcontrol>
+        </div>
+        <div @click.stop.prevent="addFirst(food, $event)" class="buy" v-show="!food.count || food.count === 0">加入购物车</div>
       </div>
-      <div class="cartcontrol-wrapper">
-        <cartcontrol :food="food"></cartcontrol>
+      <split></split>
+      <div class="info" v-show="food.info">
+        <h1 class="title">商品信息</h1>
+        <p class="text">{{food.info}}</p>
       </div>
-      <div @click.stop.prevent="addFirst(food, $event)" class="buy" v-show="!food.count || food.count === 0">加入购物车</div>
+      <split></split>
+      <div class="rating">
+        <h1 class="title">商品评价</h1>
+        <ratingselect :select-type="selectType" :only-content="onlyContent" :desc="desc" :ratings="food.ratings"></ratingselect>
+      </div>
     </div>
-  </div>
 </template>
 
 <script type="text/ecmascript-6">
 import BScroll from 'better-scroll';
 import Vue from 'vue';
 import cartcontrol from 'components/cartcontrol/cartcontrol';
+import ratingselect from 'components/ratingselect/ratingselect';
+import split from 'components/split/split';
+
+// const POSITIVE = 0;
+// const NEGATIVE = 1;
+const ALL = 2;
 
 export default {
   props: {
@@ -38,12 +53,21 @@ export default {
   },
   data() {
     return {
-      showFlag: false
+      showFlag: false,
+      selectType: ALL,
+      onlyContent: true,
+      desc: {
+        all: '全部',
+        positive: '推荐',
+        negative: '吐槽'
+      }
     };
   },
   methods: {
     show() {
       this.showFlag = true;
+      this.selectType = 0;
+      this.onlyContent = true;
       this.$nextTick(() => {
         // nextTick保证高度是被渲染的。
         if (!this.scroll) {
@@ -67,7 +91,9 @@ export default {
     }
   },
   components: {
-    cartcontrol
+    cartcontrol,
+    split,
+    ratingselect
   }
 };
 </script>
@@ -107,6 +133,7 @@ export default {
           font-size: 20px
           color: #fff
     .content
+      position: relative
       padding: 18px
       .title
         line-height: 14px
@@ -124,7 +151,6 @@ export default {
           color: rgb(147,153,159)
         .sell-count
           margin-right: 12px
-
       .price
         font-weight: 700
         line-height: 24px
@@ -136,22 +162,40 @@ export default {
           text-decoration: line-through
           font-size: 10px
           color: rgb(147,153,159)
-    .cartcontrol-wrapper
-      position: absolute
-      right: 12px
-      bottom: 12px
-    .buy
-      position: absolute
-      right: 18px
-      bottom: 18px
-      z-index: 10
-      height: 24px
-      line-height: 24px
-      padding: 0 12px
-      box-sizing: border-box
-      border-radius: 12px
-      font-size: 10px
-      color: #fff
-      background: rgb(0, 160, 220)
-
+      .cartcontrol-wrapper
+        position: absolute
+        right: 12px
+        bottom: 12px
+      .buy
+        position: absolute
+        right: 18px
+        bottom: 18px
+        z-index: 10
+        height: 24px
+        line-height: 24px
+        padding: 0 12px
+        box-sizing: border-box
+        border-radius: 12px
+        font-size: 10px
+        color: #fff
+        background: rgb(0, 160, 220)
+    .info
+      padding: 18px
+      .title
+        line-height: 14px
+        margin-bottom: 6px
+        font-size: 14px
+        color: rgb(7, 17, 27)
+      .text
+        line-height: 24px
+        padding: 0 8px
+        font-size: 12px
+        color: rgb(77, 85, 93)
+    .rating
+      padding-top: 18px
+      .title
+        line-height: 14px
+        margin-left: 18px
+        font-size: 14px
+        color: rgb(7, 17, 27)
 </style>
